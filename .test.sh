70e03c64f59c7dbd52cb4729bb3829ca1d1b0299
Bradley Jiang <qimingj@google.com>
2017-05-02 16:49:41 -0700

    Correct notebook name to exclude for testing.

--- .test.sh
+++ .test.sh
@@ -45,7 +45,7 @@ EXCLUDE_NOTEBOOKS=(
 "Getting started.ipynb"
 "Introduction to Python.ipynb"
 "UDFs using Code in Cloud Storage.ipynb"
-"LSTM Punctuation Model With TensorFlow"  # This notebook takes 30 minutes to execute.
+"LSTM Punctuation Model With TensorFlow.ipynb"  # This notebook takes 30 minutes to execute.
 )
 
 # samples/ML Toolbox/Classification/* is tested.
